b036592ea3601fe21d3de1fea26b61f505422c93
Sebastian Danielsson <sebastian.danielsson@proton.me>
2026-01-18 00:03:07 +0100

    Enable stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  extends: ["stylelint-config-standard", "stylelint-config-html/html"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-html/html",
+    "@dreamsicle.io/stylelint-config-tailwindcss",
+  ],
   rules: {
     "at-rule-no-unknown": [
       true,
